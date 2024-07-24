d16f479a7eeafc6220fa3110c3641df56847659d
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-07-24 03:34:35 +0300

    created check-ins

---
+++ src/app/components/pages/check-ins-bookings/check-ins-bookings.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PageEvent } from '@angular/material/paginator';
+
+// Components
+import { CheckInsBookingsFilterComponent } from './check-ins-bookings-filter/check-ins-bookings-filter.component';
+import { CheckInsBookingsListComponent } from './check-ins-bookings-list/check-ins-bookings-list.component';
+
+// Types
+import { CheckInBooking } from './check-ins-bookings.types';
+
+// Services
+import { CheckInsBookingsService } from './check-ins-bookings.service';
+
+// Etc
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+
+
+@Component({
+  selector: 'app-check-ins-bookings',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatProgressSpinnerModule,
+    FormsModule,
+    CheckInsBookingsFilterComponent,
+    CheckInsBookingsListComponent,
+    MatPaginatorModule
+  ],
+  templateUrl: './check-ins-bookings.component.html',
+  styleUrls: ['./check-ins-bookings.component.css']
+})
+export class CheckInsBookingsComponent implements OnInit {
+  checkInsBookings: CheckInBooking[] = [];
+  loading: boolean = false;
+  paginationInfo: any = {};
+  currentPage: number = 1;
+  perPage: number = 6;
+
+  constructor(private checkInsBookingsService: CheckInsBookingsService) {}
+
+  ngOnInit() {
+    this.checkInsBookingsService.loading$.subscribe(loading => this.loading = loading);
+    this.checkInsBookingsService.checkInsBookings$.subscribe(checkInsBookings => {
+      this.checkInsBookings = checkInsBookings;
+    });
+    this.checkInsBookingsService.paginationInfo$.subscribe(paginationInfo => {
+      this.paginationInfo = paginationInfo;
+      this.perPage = paginationInfo.perPage;
+    });
+    this.loadCheckIns();
+  }
+
+  loadCheckIns(page: number = this.currentPage) {
+    const filter = this.checkInsBookingsService.getFilter();
+    this.checkInsBookingsService.fetchCheckIns(page, this.perPage, filter);
+  }
+
+  nextPage() {
+    if (this.paginationInfo.hasNextPage) {
+      this.currentPage++;
+      this.loadCheckIns(this.currentPage);
+    }
+  }
+
+  previousPage() {
+    if (this.paginationInfo.hasPreviousPage) {
+      this.currentPage--;
+      this.loadCheckIns(this.currentPage);
+    }
+  }
+
+  onPageChange(event: PageEvent) {
+    this.currentPage = event.pageIndex + 1;
+    this.perPage = event.pageSize;
+    this.loadCheckIns(this.currentPage);
+  }
+}
---
+++ src/app/components/pages/check-ins-bookings/check-ins-bookings.types.ts
@@ -0,0 +1,69 @@
+export interface CheckInBooking {
+  _id: string;
+  client: {
+    first_name: string;
+    middle_name?: string;
+    last_name: string;
+  };
+  room: {
+    room_number: number;
+  };
+  check_in_date: Date;
+  check_out_date: Date;
+  note?: string;
+  isCheckIn: boolean;
+  totalDayPrice: number;
+  totalPrice: number;
+}
+
+export interface CheckInsBookingsApiResponse {
+  status: number;
+  message: string;
+  data: CheckInBooking[];
+}
+
+export interface CheckInsBookingsPaginationApiResponse {
+  status: number;
+  message: string;
+  data: {
+    checkIns: CheckInBooking[];
+    page: number;
+    perPage: number;
+    totalItems: number;
+    totalPages: number;
+    hasPreviousPage: boolean;
+    hasNextPage: boolean;
+  };
+}
+
+export interface CheckInAndBookingData {
+  last_name: string;
+  first_name: string;
+  middle_name?: string;
+  passport_details: string;
+  room: string;
+  isCheckIn: boolean;
+  check_in_date: Date;
+  check_out_date: Date;
+  discounts: {
+    regularCustomer: number;
+    military: number;
+    guardian: number;
+
+  },
+  totalDiscount: number;
+  totalDayPrice: number;
+  totalPrice: number;
+  comment?: string;
+  note?: string;
+}
+
+
+export interface CheckInBookingApiResponse {
+  status: number;
+  message: string;
+  data: {
+    checkIn: CheckInBooking
+}
+}
+
