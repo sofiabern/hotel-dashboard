f9c44e9677e9194f9d1d0d6b9dbcbfdc57a5f306
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-07-24 03:29:46 +0300

    updated config

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# HotelDashboard
+# Hotel Management Dashboard Project
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.1.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.6.
 
 ## Development server
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AuthInterceptor } from './auth/interceptor'
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([AuthInterceptor])),
+    provideAnimationsAsync(),
+    provideToastr(),
+    provideAnimations()
+  ]
 };
